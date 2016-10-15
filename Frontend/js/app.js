0742dd412eac6632a7c61a515686edb6c725cded
arafin9 <arafinsaad@gmail.com>
2016-10-15 18:01:32 +0600

    Updated

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -9,7 +9,30 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
       controller: 'BlogController',
       templateUrl: 'views/blog.html'
     };
+    var addPost = {
+    	name: 'post',
+    	url:'/post',
+    	controller: 'BlogController',
+    	templateUrl: 'views/post.html'
+    };
+    var detailsPost ={
+        name: 'singlepost',
+        url:'/posts/:id',
+        controller:'BlogController',
+        templateUrl: 'views/posts.html'
+    }; 
+   var commentsPost ={
+         name: 'reply',
+        url:'/posts/:id/comments/:comments_id/replys',
+        controller:'BlogController',
+        templateUrl: 'views/posts.html'
+    }
+
 
     $stateProvider.state(blogState);
+    $stateProvider.state(addPost);
+    $stateProvider.state(detailsPost);
+        $stateProvider.state(commentsPost);
+
     $urlRouterProvider.otherwise('/blog');
 })  
--- Frontend/js/controllers/BlogController.js
+++ Frontend/js/controllers/BlogController.js
@@ -1,75 +1,70 @@
-
-	myApp.controller('BlogController',['$scope','$http',function($scope,$http) {
-    	
+myApp.controller('BlogController',['$scope','$http','$stateParams','$state',function($scope,$http,$stateParams,$state) {
     	$scope.titles = "AngularJS Blog App";
-    
-      
-    //Adding Post
-    	$scope.get =function(){
+
+    //Show ALl Post
 			$http.get('http://localhost:3000/api/posts').success(function(data){
-			$scope.posts = data;
-			console.log(data)
-		});
-    	}
-    	
+            $scope.posts = data;
+            console.log(data)
+        });
+   
             $scope.postForm={
                 title: '',
                 description: ''
             };
-            
-        
-    	$scope.addPost = function(postData){
-    		$http.post('http://localhost:3000/api/posts',postData).success(function(data){
-    		$scope.post = data;
-    		});
-    	}
 
-        //Adding Comment
+    	   $scope.postID = $stateParams.id;
+            $http.get('http://localhost:3000/api/posts/'+ $scope.postID)
+            .success(function(data){
+                console.log(data);
+                $scope.spost= data;
+                
+            });
 
-        $scope.getComment =function(){
-            $http.get('http://localhost:3000/api/posts/:post_id/comments/').success(function(data){
-            $scope.comments = data;
-            console.log(data)
-        });
-        }
 
-        $scope.commentForm ={
-            comment: '',
-            post_id: ''
-            
-        };
-        
 
-        $scope.postComment = function(postComment){
-            $http.post('http://localhost:3000/api/posts/' + $scope.commentForm.post_id + '/comments',postComment).success(function(data){
+            ////comment
+            $scope.commentForm={
+                comment: ''
+            };
+
+            $scope.postComment = function(commentData){
+            $http.post('http://localhost:3000/api/posts/' + $scope.postID + '/comments',commentData).success(function(data){
                 $scope.commentData = data;
                 console.log(data);
+                
+                
+
             });
         }
+/////////////
 
 
-
-        //Adding Reply
-       
-        $scope.getreply = function(){
-            $http.get('http://localhost:3000/api/posts/:post_id/comments/:comment_id/replys').success(function(data){
-                $scope.reply = data;
+        $scope.commentId =$stateParams.comments_id;
+ 
+            $http.get('http://localhost:3000/api/posts/'+$scope.postID+'/comments/'+$scope.commentId+'/replys').success(function(data){
+                $scope.replyData = data;
             });
-        }
+            
+            //Reply
 
-        $scope.replyData ={
-            reply: '',
-            comment_id: '',
-            post_id: ''
-        };
+        
+            $scope.replyForm = 
+            {
+                reply: ''
+            };
 
-            $scope.postReply = function(postR){
-            $http.post('http://localhost:3000/api/posts/'+ $scope.replyData.post_id +'/comments/'+$scope.replyData.comment_id+'/replys',postR).success(function(data){
+            $scope.postReply = function(replyData){
+            $http.post('http://localhost:3000/api/posts/'+ $scope.postID +'/comments/'+$scope.commentId+'/replys',replyData).success(function(data){
                 $scope.replys = data;
                 console.log(data);
             });
         }
-
-
+       /////////////////
+             $scope.addPost = function(postData){
+              $http.post('http://localhost:3000/api/posts',postData).success(function(data){
+                $scope.post = data;
+            
+            });
+        }
 	}
    ]);
