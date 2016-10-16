fb0512c459da3006e10c3d3d1b9411251912fb90
arafin9 <arafinsaad@gmail.com>
2016-10-16 13:18:36 +0600

    Routes updated

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -2,18 +2,10 @@ Rails.application.routes.draw do
 	namespace:api , defaults: { format: :json } do
 	 resources :posts do 
 	 	resources :comments do 
-	 		resources :replys 
+	 		resources :replies
 	 	end
 	 end
 end
 
-	 match 'post/:id' => 'posts#show', via: [:get]
-	 match 'post/:id' => 'posts#create', via: [:post]
-	 match 'post/:id' => 'posts#update', via: [:get, :post]
-	 match 'comment/:id' => 'comments#show', via: [:get]
-	 match 'comment/:id' => 'comments#create', via: [:post]
-	 match 'comment/:id' => 'comments#update', via: [:get, :post]
-	 match 'reply/:id' => 'replys#show', via: [:get]
-	 match 'reply/:id' => 'replys#create', via: [:post]
-	 match 'reply/:id' => 'replys#update', via: [:get, :post]
+	
 end
